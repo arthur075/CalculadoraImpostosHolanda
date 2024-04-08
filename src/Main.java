a2889a9ee76381bcdad9380fda0f329bea4c5577
Arthur Matos <matossilvaarthur71@gmail.com>
2024-04-08 11:22:39 -0300

    fistCommit

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        /*Dado um determinado salarario, eu quero saber qual o valor que eu tenho que pegar
+        * dado determinado salario anual eu quero saber o valor que eu vou ter que pagar
+        * up to € 75,518	36,97% (36,93% in 2023)
+          from € 75,518	49,50% (same as in 2023) */
+
+        double valorAnual, valorMensal, valorDiario, valorSemanal;
+        int escolhaUsu;
+        double impostoMin;
+        double resultado;
+        double impostoMax;
+
+        System.out.println("Insira de que forma você deseja informar o seu salário: ");
+        System.out.println("Diario(1) Semanal(2) Mensal(3) Anual(4)");
+        Scanner input = new Scanner(System.in);
+        escolhaUsu = input.nextInt();
+
+        switch (escolhaUsu) {
+
+            case 1:
+                System.out.println("Digite o seu salário diário: ");
+                valorDiario = input.nextDouble();
+                resultado = calculaDiario(valorDiario);
+                System.out.println("O seu salário de: " + valorDiario + " por dia, com os impostos ficara de: " + resultado + " por ano");
+                break;
+            case 2:
+                System.out.println("Digite o seu salário semanal: ");
+                valorSemanal = input.nextDouble();
+                resultado = calculaSemanal(valorSemanal);
+                System.out.println("O seu salario de:");
+
+        }
+
+
+    }
+    public static double calculaDiario (double valorDiario) {
+        /*necessidade: multiplicar por 30, 12 taxa */
+        double resultado;
+        resultado = ((valorDiario * 30) * 12);
+        if (resultado <= 75518) {
+            resultado = (resultado * 36.97) / 100;
+        } else if (resultado > 75518) {
+            resultado = (resultado * 49.50) / 100;
+        }
+
+
+        return resultado;
+    }
+
+    public static double calculaSemanal (double valorSemanal) {
+
+        double resultado;
+        resultado = ((valorSemanal * 4) * 12);
+        if (resultado <= 75518) {
+            resultado = (resultado * 36.97) / 100;
+        } else if (resultado > 75518) {
+            resultado = (resultado * 49.50) / 100;
+        }
+
+
+        return resultado;
+
+    }
+
+}
